952f7f58c026ca97cb6340a4ef988b4ada6e6656
phinaphires <138526348+phinaphires@users.noreply.github.com>
2023-07-03 21:32:28 +0100

    Add slack message listener

---
+++ slack_message_listener.py
@@ -0,0 +1,5 @@
+# write a python script that use a slack api token to connect to a slack channel
+# and listen for messages
+# when a message is received, send it to the message handler
+# for processing
+
